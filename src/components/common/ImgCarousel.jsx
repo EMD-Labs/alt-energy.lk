d8681983553b4dad16d210a6b4941e2be3d87222
aslam jm <aslamjm@aslams-MacBook-Pro.local>
2022-03-31 15:20:10 +0530

    new commit

--- src/components/common/ImgCarousel.jsx
+++ src/components/common/ImgCarousel.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import "react-responsive-carousel/lib/styles/carousel.min.css"; 
 import { Carousel } from 'react-responsive-carousel';
 import { GatsbyImage } from "gatsby-plugin-image"
+import * as styles from '../../styles/carousel.module.scss'
 
 const ImgCarousel = ({images}) => {
 console.log(images);
   return (
-    <Carousel showThumbs={true} autoPlay>
+    <Carousel autoPlay>
         {
-            images.map((img,index)=><div key={index}>
+            images.map((img,index)=><div key={index} className={styles.imgCtr}>
                 <GatsbyImage image={img.childrenImageSharp[0].gatsbyImageData}/>
             </div>)
         }
